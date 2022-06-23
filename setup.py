5fe4c3925caa020ab5452c47079f5810856acd49
krishna <krishnakalyan3@gmail.com>
2022-06-23 14:16:35 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,37 @@
 
 from setuptools import find_packages, setup
 
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+with open("requirements.txt") as _file:
+    install_reqs = []
+    dependency_links = []
+    for req in _file.readlines():
+
+        if req.startswith("--extra-index-url"):
+            dependency_links.append(req.replace("--extra-index-url ", ""))
+        else:
+            install_reqs.append(req)
+
+with open("tests/requirements.txt") as _file:
+    test_reqs = [req for req in _file.readlines()]
+
 setup(
-    name="lit_jupyter",
-    version="0.0.0",
-    description="⚡ Lightning component ⚡ generated with command: lightning init component",
-    author="",
-    author_email="",
-    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url="https://github.com/PyTorchLightning/lightning-component-template",
-    install_requires=[],
-    packages=find_packages(),
+    name="lit_telegram",
+    version="0.0.1",
+    description="Messaging notifications for telegram.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Krishna, William, Marc, Thomas",
+    author_email="krishna@grid.ai",
+    url="https://github.com/Lightning-AI/LAI-Jupyter-Component",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=install_reqs,
+    dependency_links=dependency_links,
+    extras_require={
+        "test": test_reqs,
+    },
+    python_requires=">=3.7",
 )
