19afebc8921d86d0bf2f27ad1748da6a71034708
krishna <krishnakalyan3@gmail.com>
2022-06-24 14:31:03 +0200

    refactor and fix logs

--- lit_jupyter/component.py
+++ lit_jupyter/component.py
@@ -5,24 +5,31 @@ import os
 import lightning as L
 from typing import Optional
 from pathlib import Path
-from subprocess import Popen, PIPE, STDOUT
 
 
 class LitJupyter(L.LightningWork):
     def __init__(self, cloud_compute: Optional[L.CloudCompute] = None):
         super().__init__(cloud_compute=cloud_compute, parallel=True)
+        self.exit_code = None
+        self.storage = None
 
-    
     def run(self):
+        jupyter_out = open('jupyter_log.txt', 'a')
+
         # Delete Existing Configuration
         jupyter_notebook_config_path = Path.home() / ".jupyter/jupyter_notebook_config.py"
         if os.path.exists(jupyter_notebook_config_path):
             os.remove(jupyter_notebook_config_path)
 
         # Generate new configuration
-        cmd_config = f"{sys.executable} -m notebook --generate-config"
-        Popen(cmd_config, shell=True)
-
+        proc_config =  subprocess.Popen(
+            f"{sys.executable} -m notebook --generate-config".split(" "),
+            stdout=jupyter_out,
+            stderr=jupyter_out,
+            bufsize=0,
+            close_fds=True)
+        proc_config.wait()
+  
         # Add iFrame configuration
         with open(jupyter_notebook_config_path, "a") as f:
             f.write(
@@ -31,7 +38,11 @@ class LitJupyter(L.LightningWork):
 
         # Start jupyter without password
         remove_pwd = f"--NotebookApp.token='' --NotebookApp.password=''"
-        cmd_server = f"{sys.executable} -m jupyter lab --ip {self.host} --port {self.port} --no-browser {remove_pwd}"
-        subprocess.run(cmd_server, shell=True)
-
-        logging.info(f'Notebook Stated')
+        proc_server = subprocess.Popen(
+            f"{sys.executable} -m jupyter lab --ip {self.host} --port {self.port} --no-browser {remove_pwd}".split(" "),
+            stdout=jupyter_out,
+            stderr=jupyter_out,
+            bufsize=0,
+            close_fds=True
+        )
+        proc_server.wait()
