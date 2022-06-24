0076d09bb71f9281a877675926501c740b94c7b5
krishna <krishnakalyan3@gmail.com>
2022-06-24 14:39:39 +0200

    update

--- tests/test_lit_jupyter_component.py
+++ tests/test_lit_jupyter_component.py
@@ -2,9 +2,9 @@ from unittest import mock
 from lit_jupyter.component import LitJupyter
 
 
-@mock.patch("lit_jupyter.component.subprocess.run")
+@mock.patch("lit_jupyter.component.subprocess.Popen")
 def test_subprocess(mock_popen):
     mock_popen().wait.return_value = 0
-    obj1 = LitJupyter()
-    obj1.run()
+    lightning_work = LitJupyter()
+    lightning_work.run()
     return mock_popen.assert_called()
