382fe9309e5d11e6bf65115dc4a1c6416762b04b
krishna <krishnakalyan3@gmail.com>
2022-07-01 11:36:57 +0200

    ci demo

--- demo_app.py
+++ demo_app.py
@@ -6,10 +6,11 @@ import os
 class JupyterLabManager(L.LightningFlow):
     def __init__(self) -> None:
         super().__init__()
-        self.jupyter_work = JupyterLab(cloud_compute=L.CloudCompute(os.getenv("COMPUTE", "cpu-small"), idle_timeout=0), kernel="python|r|julia")
+        self.jupyter_work = JupyterLab(cloud_compute=L.CloudCompute(os.getenv("COMPUTE", "cpu-small")), kernel="python|r|julia")
 
     def run(self):
         self.jupyter_work.run()
+        self._exit("Application End!")
     
     def configure_layout(self):
         return [{'name': "JupyterLab", 'content': self.jupyter_work}]
