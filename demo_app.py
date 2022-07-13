689ac6868668c1961e07cc3516e6f992818a9c4e
krishna <krishnakalyan3@gmail.com>
2022-07-13 10:21:18 +0200

    update code

--- demo_app.py
+++ demo_app.py
@@ -7,12 +7,13 @@ from lit_jupyter import JupyterLab
 class JupyterLabManager(L.LightningFlow):
     def __init__(self) -> None:
         super().__init__()
-        self.jupyter_work = JupyterLab(cloud_compute=L.CloudCompute(os.getenv("COMPUTE", "cpu-small")), kernel="python|r|julia")
+        self.jupyter_work = JupyterLab(cloud_compute=L.CloudCompute(os.getenv("COMPUTE", "cpu-small")), kernel="python")
 
     def run(self):
         self.jupyter_work.run()
-        if os.environ.get('TEST'):
-            self._exit("Application End!")
+        if self.jupyter_works.token:
+            print("Notebook Deployed")
+
     
     def configure_layout(self):
         return [{'name': "JupyterLab", 'content': self.jupyter_work}]
