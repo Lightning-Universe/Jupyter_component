9bc0b2210851e4ede5dfda2407b3d1f80a726021
Jirka <jirka.borovec@seznam.cz>
2022-09-23 19:53:26 +0200

    code

--- demo_app.py
+++ demo_app.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
 import os
+
 import lightning as L
+
 from lai_jupyter import JupyterLab
+
 
 class JupyterLabManager(L.LightningFlow):
     def __init__(self) -> None:
         super().__init__()
-        self.jupyter_work = JupyterLab(kernel=os.getenv("LIGHTNING_JUPYTER_LAB_KERNEL", "python"), cloud_compute=L.CloudCompute(os.getenv("LIGHTNING_JUPYTER_LAB_COMPUTE", "cpu-small")))
+        self.jupyter_work = JupyterLab(
+            kernel=os.getenv("LIGHTNING_JUPYTER_LAB_KERNEL", "python"),
+            cloud_compute=L.CloudCompute(os.getenv("LIGHTNING_JUPYTER_LAB_COMPUTE", "cpu-small")),
+        )
 
     def run(self):
         self.jupyter_work.run()
-    
+
     def configure_layout(self):
-        return [{'name': "JupyterLab", 'content': self.jupyter_work}]
+        return [{"name": "JupyterLab", "content": self.jupyter_work}]
+
 
 app = L.LightningApp(JupyterLabManager())
